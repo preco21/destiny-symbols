a4e35d242fe916d996a800b5b55f827ace044fd7
Plusb Preco <plusb21@gmail.com>
2020-01-05 15:29:34 +0900

    Meta tweak for housekeeping

--- src/next-env.d.ts
+++ src/next-env.d.ts
@@ -1,12 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
-declare global {
-  import 'styled-components/cssprop'
-
-  module '*.png' {
-    export default '' as string
-  }
+declare module '*.png' {
+  export default '' as string
 }
-
-export {}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import { Layout } from '../layouts/Layout'
 
 export default function IndexPage() {
-  return <Layout />
+  return <Layout>foobar</Layout>
 }
