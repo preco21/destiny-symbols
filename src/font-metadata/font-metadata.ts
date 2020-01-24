bed26b94f83a121a3ccb3d10796e7247264af0b4
Plusb Preco <plusb21@gmail.com>
2020-01-25 05:01:14 +0900

    Update api links

--- src/font-metadata/font-metadata.ts
+++ src/font-metadata/font-metadata.ts
@@ -1,39 +1,25 @@
 import axios from 'axios'
 import { apibase } from '../utils/api'
 
-export interface FontMetadataLink {
+export type IGlyph = Partial<opentype.Glyph> & { character: string }
+
+export interface IFontMetadata {
   name: string
   filename: string
-  metadataPath: string
+  fontFamily: string
   totalGlyphs: number
 }
 
-export interface FontMetadataList {
+export interface IFontCombined {
+  meta: IFontMetadata[]
+  totalGlyphs: number
+  glyphs: IGlyph[]
   createdAt: string
-  dir: string
-  fonts: FontMetadataLink[]
 }
 
-export interface FontMetadata {
-  index: number
-  name: string
-  unicodes: number[]
-  advanceWidth: number
-  leftSideBearing: number
-  character: string
-  unicode?: number
-}
-
-export async function loadMetadataList() {
+export async function loadMetadata() {
   const { data: fontMetadataList } = await axios.get(
-    apibase('font-meta/resources/fonts-export/meta.json'),
+    apibase('resources/fonts-export/meta.json'),
   )
-  return fontMetadataList as FontMetadataList
-}
-
-export async function loadMetadata(path: string) {
-  const { data: fontMetadata } = await axios.get(
-    apibase(`font-meta/resources/fonts-export/${path}`),
-  )
-  return fontMetadata as FontMetadata[]
+  return fontMetadataList as IFontCombined
 }
