f40c7901af7c236e7fd7fbc0417bab47755e1e6d
Plusb Preco <plusb21@gmail.com>
2020-01-25 04:21:45 +0900

    Disable copying font files until automatic font profile generation feature lands

--- internals/font.ts
+++ internals/font.ts
@@ -7,7 +7,7 @@ const FONT_INCLUDES = ['Destiny_Keys.otf', 'Destiny_Symbols_PC.otf']
 
 const SOURCE_PATH = 'resources/fonts'
 const METADATA_OUTPUT_PATH = 'resources/fonts-export/meta.json'
-const FONT_OUTPUT_PATH = 'resources/fonts-export/fonts'
+  // const FONT_OUTPUT_PATH = 'resources/fonts-export/fonts'
 
   // prettier-ignore
 ;(async () => {
@@ -22,15 +22,16 @@ const FONT_OUTPUT_PATH = 'resources/fonts-export/fonts'
       METADATA_OUTPUT_PATH,
       JSON.stringify(fontExtract.getMergedFont()),
     )
-    await ensureDir(FONT_OUTPUT_PATH)
-    await Promise.all(
-      FONT_INCLUDES.map((fontFilename) =>
-        copy(
-          join(SOURCE_PATH, fontFilename),
-          join(FONT_OUTPUT_PATH, fontFilename),
-        ),
-      ),
-    )
+    // FIXME disabled until it supports automatic custom font profile generation
+    // await ensureDir(FONT_OUTPUT_PATH)
+    // await Promise.all(
+    //   FONT_INCLUDES.map((fontFilename) =>
+    //     copy(
+    //       join(SOURCE_PATH, fontFilename),
+    //       join(FONT_OUTPUT_PATH, fontFilename),
+    //     ),
+    //   ),
+    // )
   } catch (err) {
     console.error(err)
     process.exit(1)
