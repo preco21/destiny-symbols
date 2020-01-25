8f7d6807065cacf94babc6089f258478007ee9af
Plusb Preco <plusb21@gmail.com>
2020-01-25 10:12:57 +0900

    Update api utils

--- src/font-metadata/font-metadata.ts
+++ src/font-metadata/font-metadata.ts
@@ -1,12 +1,24 @@
+import { basename, extname } from 'path'
 import axios from 'axios'
 import { apibase } from '../utils/api'
 
-export type IGlyph = Partial<opentype.Glyph> & { character: string }
+export type IGlyph = Partial<opentype.Glyph> & {
+  index: number
+  character: string
+}
 export type IGlyphWithMetadata = IGlyph & {
   meta: {
     name: string
     filename: string
     fontFamily: string
+  }
+}
+export type IGlyphWithMetadataDistinct = IGlyph & {
+  meta: {
+    name: string
+    filename: string
+    fontFamily: string
+    distinctFontFamily: string
   }
 }
 
@@ -17,16 +29,34 @@ export interface IFontMetadata {
   totalGlyphs: number
 }
 
-export interface IFontCombined {
+export interface IFontCombinedBase<T> {
   meta: IFontMetadata[]
   totalGlyphs: number
-  glyphs: IGlyphWithMetadata[]
+  glyphs: T[]
   createdAt: string
 }
+export type IFontCombined = IFontCombinedBase<IGlyphWithMetadata>
+export type IFontCombinedWithDistinctFontFamily = IFontCombinedBase<
+  IGlyphWithMetadataDistinct
+>
 
-export async function loadMetadata() {
-  const { data: fontMetadataList } = await axios.get(
+export async function loadMetadata(): Promise<
+  IFontCombinedWithDistinctFontFamily
+> {
+  const { data: fontMetadataList } = await axios.get<IFontCombined>(
     apibase('resources/fonts-export/meta.json'),
   )
-  return fontMetadataList as IFontCombined
+  return {
+    ...fontMetadataList,
+    glyphs: fontMetadataList.glyphs.map((glyph) => ({
+      ...glyph,
+      meta: {
+        ...glyph.meta,
+        distinctFontFamily: basename(
+          glyph.meta.filename,
+          extname(glyph.meta.filename),
+        ),
+      },
+    })),
+  }
 }
